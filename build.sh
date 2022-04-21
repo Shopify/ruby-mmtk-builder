f9c54b8258166ff92e7c0f66f763dbce7f5229fe
Kunshan Wang <wks1986@gmail.com>
2022-04-22 00:50:11 +0800

    Change for ./configure args
    Now --with-mmtk-ruby=/path/to/mmtk-ruby must point to the mmtk-ruby
    repository.  It now searches for the release build of mmtk-ruby by
    default.  Now it uses rpath to locate libmmtk-ruby.so, therefore
    LD_LIBRARY_PATH is no longer necessary, and it is also not necessary to
    copy the .so manually.

--- build.sh
+++ build.sh
@@ -7,26 +7,27 @@ source $HOME/.cargo/env
 
 if [ -v WITH_LATEST_MMTK_CORE ]
 then
-  git clone https://github.com/mmtk/mmtk-core
-  pushd mmtk-core
+  git clone --depth=1 https://github.com/mmtk/mmtk-core
 else
-  git clone https://github.com/mmtk/mmtk-core # https://github.com/wks/mmtk-core
-  pushd mmtk-core
+  git clone --depth=1 https://github.com/mmtk/mmtk-core # https://github.com/wks/mmtk-core
   # Currently no difference
 fi
+
+# Building mmtk-core in the mmtk-core directory has no effect for mmtk-ruby.
+# When building mmtk-ruby, it will build the source of mmtk-core, too,
+# and the generated object files will be in mmtk-ruby/mmtk/target/
+
 export RUSTUP_TOOLCHAIN=nightly # $(cat rust-toolchain)
 rustup toolchain install $RUSTUP_TOOLCHAIN
-cargo +nightly build
-popd
 
-git clone https://github.com/mmtk/mmtk-ruby
+git clone --depth=1 https://github.com/mmtk/mmtk-ruby
 pushd mmtk-ruby/mmtk
 sed -i 's/^mmtk =/#mmtk =/g' Cargo.toml
 cat ../../Cargo.toml.part >> Cargo.toml
-cargo build
+cargo +nightly build --release
 popd
 
-git clone https://github.com/mmtk/ruby
+git clone --depth=1 https://github.com/mmtk/ruby
 pushd ruby
 if [ -v WITH_UPSTREAM_RUBY ]
 then
@@ -36,14 +37,13 @@ then
   git fetch upstream
   git merge upstream/master
 fi
-cp ../mmtk-ruby/mmtk/target/debug/libmmtk_ruby.so ./
 sudo apt-get install -y autoconf bison
 ./autogen.sh
-./configure --with-mmtk-ruby --prefix=$PWD/build --disable-install-doc
-export LD_LIBRARY_PATH=$PWD
+./configure --with-mmtk-ruby=../mmtk-ruby --prefix=$PWD/build --disable-install-doc
 export MMTK_PLAN=MarkSweep
-export THIRD_PARTY_HEAP_LIMIT=10000000
+export THIRD_PARTY_HEAP_LIMIT=500000000
 make miniruby -j
-export RUST_LOG=trace
+export RUST_LOG=info
 ./miniruby -e 'puts "Hello world!"'
+make install -j
 popd
